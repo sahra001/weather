c7688d7d56ffc1b6abfd42fdc4ff465fb2d4f57b
sahra001 <134542621+sahra001@users.noreply.github.com>
2024-01-10 20:52:28 +0430

    Update App.js
    add break

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h2>Weather App</h2>
       <input placeholder="Search your country" />
+      <br/>
       <a href="https://github.com/sahra001/weather">My GitHub repository</a>
     </div>
   );
